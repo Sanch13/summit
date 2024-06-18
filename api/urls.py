1f2a7243a1653cd3465d2acb8997a4fea862d73e
sanch13 <sanch13sanch@gmail.com>
2024-06-18 06:22:44 +0300

    check email by api

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 
 from api.spectacular.urls import urlpatterns as doc_urls
-from users.utils import validate_password
+from users.utils import validate_password, check_email
 
 app_name = 'api'
 
 urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path('check_pswd/', validate_password),
+    path('check_email/', check_email),
+
 ]
 
 urlpatterns += doc_urls
--- users/utils.py
+++ users/utils.py
@@ -1,20 +1,49 @@
-from django.contrib.auth.password_validation import validate_password as django_validate_password
-from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import CommonPasswordValidator
 
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class CheckCommonPassword(CommonPasswordValidator):
+    def validate(self, password, user=None):
+        return password.lower().strip() in self.passwords
 
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def validate_password(request):
-    if request.method == 'POST':
-        password = request.data.get('password')
-        try:
-            django_validate_password(password)
-            return Response({'valid': True})
-        except ValidationError as e:
-            return Response({'valid': False, 'errors': e.messages})
+    validator = CheckCommonPassword()
+    password = request.data.get('password')  # false true -- check
+
+    if not password:
+        return Response(data={'valid': False, 'error': "Поле не может быть пустым."},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    if validator.validate(password=password):
+        return Response(data={'valid': False, 'error': "Этот пароль слишком распространен."},
+                        status=status.HTTP_200_OK)
     else:
-        return Response({'error': 'Invalid request'}, status=400)
+        return Response(data={'valid': True},
+                        status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def check_email(request):
+    email = request.data.get('email')
+    if not email:
+        return Response(data={'error': 'Email is required'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    if User.objects.filter(email=email).exists():
+        return Response(data={'valid': False},
+                        status=status.HTTP_200_OK)
+    else:
+        return Response(data={'valid': True},
+                        status=status.HTTP_200_OK)
